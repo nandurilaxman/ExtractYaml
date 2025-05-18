7e6faaf86ccfcfd7254b07bcb4bf905e34a549d3
Laxman Nanduri <162944154+nandurilaxman@users.noreply.github.com>
2025-05-18 19:19:33 +0530

    Update WeatherForecastController.cs

--- src/API/Controllers/WeatherForecastController.cs
+++ src/API/Controllers/WeatherForecastController.cs
@@ -1,33 +1,57 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace MyApi.Controllers
+namespace API.Controllers;
+
+[ApiController]
+[Route("weatherforecast")]
+public class WeatherForecastController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    private static readonly string[] Summaries = new[]
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Swelterin", "Scorching"
+    };
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+    [HttpGet("rain")]
+    public IEnumerable<WeatherForecast> GetRainForecast()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = "Rainy"
+        })
+        .ToArray();
     }
 
-    public class WeatherForecast
+    [HttpGet("sun")]
+    public IEnumerable<WeatherForecast> GetSunForecast()
     {
-        public DateTime Date { get; set; }
-        public int TemperatureC { get; set; }
-        public string? Summary { get; set; }
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = "Sunny"
+        })
+        .ToArray();
     }
+
+    [HttpGet("wind")]
+    public IEnumerable<WeatherForecast> GetWindForecast()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = "Windy"
+        })
+        .ToArray();
+    }
+}
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+    public int TemperatureC { get; set; }
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public string? Summary { get; set; }
 }
